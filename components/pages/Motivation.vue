a2194074ec1b9eed4784351200121e5346316571
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-02 08:41:08 +0100

    separate motivation content from route page

---
+++ components/pages/Motivation.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="text-slate-500 flex flex-col">
+        <LayoutPageTitle>Motivation</LayoutPageTitle>
+        <LayoutContentWrapper>
+            <p>
+                I'm a web developer who's passionate about using code to create beautiful and functional websites.
+                <br />
+                <br />
+                I was hooked on coding from the moment I started learning it in university, and I've been fascinated by CSS
+                ever since. I love the way that CSS can be used to transform a plain HTML page into a visually stunning and
+                engaging experience.
+                <br />
+                That's why I closely follow industry leaders like <NuxtLink to="https://twitter.com/kevinjpowell"
+                    target="_blank">Kevin Powell</NuxtLink> and <NuxtLink to="https://twitter.com/amit_sheen"
+                    target="_blank">Amit Sheen</NuxtLink> to stay ahead of the curve.
+                <br />
+                <br />
+                I've built websites for a variety of clients, ranging from non-profits and small businesses to large scale
+                businesses.
+                <br />
+                I use my coding skills to make websites that are accessible to everyone, regardless of their technical
+                knowledge.
+                <br />
+                <br />
+                I believe that web development is a powerful tool that can be used to make a positive impact on the world.
+                I'm excited to be a part of this field, and I'm eager to see what the future holds.
+                <br />
+                <br />
+                I also love to teach tech. I've taught children and adults, formally and informally, at the office and
+                otherwise.
+                I love sharing my knowledge and helping others to learn new things. I believe that everyone has the
+                potential to learn to code, and I'm passionate about making that happen.
+            </p>
+
+
+        </LayoutContentWrapper>
+        <div class="w-full py-4">
+            <LinkButton label="Experience" href="/experience" />
+        </div>
+    </div>
+</template>
