06df394e1da58a408048d73c44e04bf311173057
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-02 08:39:46 +0100

    wrap nav view in a component

---
+++ components/layout/NavViewWrapper.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const isNavOpen = inject<boolean>("isNavOpen");
+const onNavClose = inject<() => void>("onNavClose");
+
+</script>
+
+<template>
+    <div class="relative flex flex-col flex-1 h-full overflow-hidden">
+        <LayoutNavItem v-show="isNavOpen" />
+        <div class="p-6 pb-0 rounded-3xl transition-all duration-300 ease-[cubic-bezier(0,1,0.7,1.02)] overflow-hidden flex-1 [&>div]:h-full [&>div]:overflow-hidden"
+            :class="{ '[&>div]:pointer-events-none !duration-700 scale-[0.65] backdrop-blur-3xl -mr-10 ml-10 bg-slate-500 bg-opacity-10 shadow-2xl': isNavOpen }"
+            @click="isNavOpen && onNavClose && onNavClose()">
+            <slot />
+        </div>
+    </div>
+</template>
