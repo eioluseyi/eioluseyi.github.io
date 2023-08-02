e5f9760cc27b26bf627f63b0f81c525c0e317ff4
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-02 08:38:36 +0100

    add a content wrapper component

---
+++ components/layout/ContentWrapper.vue
@@ -0,0 +1,23 @@
+<template>
+    <div
+        class="flex-1 overflow-hidden [&_a]:ease-out [&_a]:transition-all [&_a]:duration-300 hover:overflow-auto py-5 [&_a]:whitespace-nowrap [&_a]:text-slate-300 [&_a:hover]:text-slate-400 content-wrapper">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.content-wrapper {
+    scrollbar-gutter: stable;
+
+    --mask-gradient: transparent, black 30px calc(100% - 30px), transparent;
+    -webkit-mask-image: linear-gradient(var(--mask-gradient));
+    mask-image: linear-gradient(var(--mask-gradient));
+
+    cursor: default;
+}
+
+
+::-webkit-scrollbar-thumb {
+    border-width: 27px 2px 27px 0px;
+}
+</style>
