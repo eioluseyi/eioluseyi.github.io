98d880ffc5ba56275b83a3aecdd6297b813f8e52
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-02 08:40:35 +0100

    separate experience content from route page

---
+++ components/pages/Experience.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="text-slate-500">
+        <LayoutPageTitle>Experience</LayoutPageTitle>
+        <LayoutContentWrapper>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab doloribus recusandae, maxime maiores optio inventore
+            eum fugit magnam ratione ex cumque hic ut laborum ea ipsum facere. Sunt, necessitatibus cum.
+            <br><br>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet minima obcaecati accusantium beatae
+            corrupti, commodi minus numquam. Officiis libero dolore neque ad assumenda totam possimus voluptas placeat
+            expedita laboriosam?
+        </LayoutContentWrapper>
+    </div>
+</template>
