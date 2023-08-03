5d60a39355c8615c5ed1f88fdff4fc82266067a6
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-03 05:35:42 +0100

    begin nav view

--- components/layout/NavItem.vue
+++ components/layout/NavItem.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 
+const router = useRouter();
+
 const isNavOpen = inject<boolean>("isNavOpen");
 const onNavClose = inject<() => void>("onNavClose");
+
+const loadPage = () => {
+    if (isNavOpen && onNavClose) {
+        router.push("/motivation");
+        onNavClose();
+    }
+}
 
 </script>
 
 <template>
-    <div class="absolute inset-0 -left-10 p-6 pb-0 rounded-3xl transition-all duration-300 ease-[cubic-bezier(0,1,0.7,1.02)] overflow-hidden flex-1 [&>div]:h-full [&>div]:overflow-hidden"
-        :class="{ '[&>div]:pointer-events-none !duration-700 scale-[0.6] bg-slate-500 bg-opacity-10 shadow-2xl': isNavOpen }"
-        @click="isNavOpen && onNavClose && onNavClose()">
-        Nav items-center
+    <div class="min-w-full translate-x-0 p-6 pb-0 rounded-3xl transition-all duration-300 ease-[cubic-bezier(0,1,0.7,1.02)] overflow-hidden border-[#ffffff07] flex-1 [&>div]:h-full [&>div]:overflow-hidden"
+        :class="{ '[&>div]:pointer-events-none !duration-700 scale-[0.63] backdrop-blur-2xl blur-[1px] bg-[#1F232Ddd] border shadow-3xl': isNavOpen }"
+        @click="loadPage">
+        <LazyPagesMotivation />
     </div>
 </template>
--- components/layout/NavViewWrapper.vue
+++ components/layout/NavViewWrapper.vue
@@ -3,15 +3,22 @@
 const isNavOpen = inject<boolean>("isNavOpen");
 const onNavClose = inject<() => void>("onNavClose");
 
+const handleScroll = (evt: Event) => {
+    console.log(evt);
+}
+
 </script>
 
 <template>
-    <div class="relative flex flex-col flex-1 h-full overflow-hidden">
-        <LayoutNavItem v-show="isNavOpen" />
-        <div class="p-6 pb-0 rounded-3xl transition-all duration-300 ease-[cubic-bezier(0,1,0.7,1.02)] overflow-hidden flex-1 [&>div]:h-full [&>div]:overflow-hidden"
-            :class="{ '[&>div]:pointer-events-none !duration-700 scale-[0.65] backdrop-blur-3xl -mr-10 ml-10 bg-slate-500 bg-opacity-10 shadow-2xl': isNavOpen }"
-            @click="isNavOpen && onNavClose && onNavClose()">
-            <slot />
+    <div class="flex flex-col flex-1 h-full overflow-y-hidden overflow-x-scroll no-scrollbar" @scroll="handleScroll">
+        <div class="relative flex flex-1 h-full">
+            <LayoutNavItem v-show="isNavOpen" />
+            <div class="min-w-full p-6 pb-0 rounded-3xl transition-all duration-300 ease-[cubic-bezier(0,1,0.7,1.02)] overflow-hidden border-[#ffffff07] flex-1 [&>div]:h-full [&>div]:overflow-hidden"
+                :class="{ '[&>div]:pointer-events-none !duration-700 translate-x-10 scale-[0.65] backdrop-blur-2xl bg-[#1F232Ddd] border shadow-3xl': isNavOpen }"
+                @click="isNavOpen && onNavClose && onNavClose()">
+                <slot />
+            </div>
+            <div v-if="isNavOpen" class="min-w-0 w-full opacity-0">.</div>
         </div>
     </div>
 </template>
