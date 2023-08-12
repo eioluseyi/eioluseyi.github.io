cb16fbade2b8ad59301e55d244435126be45fbbd
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-13 00:07:06 +0100

    update experience page

--- components/pages/Experience.vue
+++ components/pages/Experience.vue
@@ -1,13 +1,48 @@
+<script setup lang="ts">
+import EXPERIENCE_LIST from "../../content/EXPERIENCE_LIST.json";
+
+const experienceList = ref<{
+    from: string;
+    to: string;
+    company: string;
+    jobTitle: string;
+    companyLogo?: string;
+    projectImage?: string;
+    location?: string;
+    jobType?: string;
+    isSelected?: boolean;
+}[]>(EXPERIENCE_LIST);
+
+const downloadResume = () => {
+    const link = document.createElement('a');
+    link.href = '/Resume.pdf';
+    link.download = 'Imolorhe_Emmanuel_Resume.pdf';
+    link.target = '_blank';
+    link.click();
+    link.remove();
+}
+</script>
+
 <template>
-    <div class="text-slate-500">
+    <div class="flex flex-col text-slate-500">
         <LayoutPageTitle>Experience</LayoutPageTitle>
-        <LayoutContentWrapper>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab doloribus recusandae, maxime maiores optio inventore
-            eum fugit magnam ratione ex cumque hic ut laborum ea ipsum facere. Sunt, necessitatibus cum.
-            <br><br>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet minima obcaecati accusantium beatae
-            corrupti, commodi minus numquam. Officiis libero dolore neque ad assumenda totam possimus voluptas placeat
-            expedita laboriosam?
+        <div class="text-sm text-slate-400 my-5">
+            Frontend Engineer with 5+ years of experience in building <span class="whitespace-nowrap">user-friendly</span>,
+            visually captivating and
+            <span class="whitespace-nowrap">high-quality</span>
+            web experiences. Proven ability to work independently and as part of a team, meeting deadlines under
+            pressure.
+        </div>
+        <LayoutContentWrapper class="">
+            <div class="py-">
+                <ExperienceItem v-for="(itm, idx) in experienceList" :item="itm" />
+            </div>
         </LayoutContentWrapper>
+        <div class="w-full py-4">
+            <SecondaryButton class="w-full bottom-full mb-2" @click="downloadResume">
+                <i class="gg-software-download mr-2" /> Resume
+            </SecondaryButton>
+            <LinkButton label="Projects" href="/projects" />
+        </div>
     </div>
 </template>
