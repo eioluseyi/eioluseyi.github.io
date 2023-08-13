95590ed826fb8fcfefbd8adfd43dae76f45c8ab9
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-13 03:36:52 +0100

    update secondary button

--- components/SecondaryButton.vue
+++ components/SecondaryButton.vue
@@ -3,7 +3,7 @@
 
 <template>
     <button
-        class="rounded-full flex items-center justify-center px-5 py-4 text-slate-300 border-2 bg-[#1D212A] border-current transition-all duration-500 ease-out font-bold">
+        class="rounded-full flex items-center justify-center px-5 py-4 text-slate-300 border-2 bg-[#1D212A] border-current transition-all duration-500 ease-out hover:tracking-widest font-bold">
         <slot />
     </button>
 </template>
