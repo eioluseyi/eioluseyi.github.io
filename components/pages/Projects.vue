24bb3bcdcc940d786a23f03605b66ac34f9217d0
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-13 04:08:37 +0100

    initialize projects route

---
+++ components/pages/Projects.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-slate-500 flex flex-col">
+        <LayoutPageTitle>Projects</LayoutPageTitle>
+        <LayoutContentWrapper>
+            <p>
+                Ha! Projector l'emi!
+            </p>
+
+
+        </LayoutContentWrapper>
+        <div class="w-full py-4">
+            <LinkButton label="Reach out" href="/react-out" />
+        </div>
+    </div>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+useSeoMeta({
+    title: 'Projects | Emmanuel Imolorhe'
+})
+</script>
+
+<template>
+    <PagesProjects />
+</template>
