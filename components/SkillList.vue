3fa99c8b044b6249c54f29017d02eb2699fb0d1f
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-17 10:05:30 +0100

    update skill components content to cater for icon only mode

--- components/SkillList.vue
+++ components/SkillList.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 type Props = {
     itemList?: SkillItemType[];
-    size?: 'small' | 'large';
+    size?: 'icon' | 'small' | 'large';
 };
 
 withDefaults(defineProps<Props>(), {
@@ -12,7 +12,7 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <div class="flex justify-center gap-4">
+    <div class="flex justify-center gap-4 max-w-[100%] overflow-auto" :class="{ '!gap-2': size === 'icon' }">
         <SkillItem v-for="itm in itemList" :item="itm" :size="size"
             :class="{ '[&:not(:last-of-type)]:pr-4 [&:not(:last-of-type)]:border-r [&:not(:last-of-type)]:border-current': size === 'small' }" />
     </div>
