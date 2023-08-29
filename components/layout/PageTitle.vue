f24cacf894173b6369e341074aaa81d507429630
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-08-29 10:51:32 +0100

    chore: reduce heading component text size for mobile

--- components/layout/PageTitle.vue
+++ components/layout/PageTitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="text-slate-200 text-[3em] font-extrabold">
+    <h1 class="text-slate-200 min-[400px]:text-[3em] max-[400px]:text-[2.5em] font-extrabold">
         <slot />
     </h1>
 </template>
