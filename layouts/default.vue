eb91745fd2a71582ab42b6d15fb7c6db86117a94
Emmanuel Imolorhe <imolorhe@gmail.com>
2023-09-22 13:02:54 +0100

    temporarily turn off shadows to improve animation performance

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,7 @@ provide('onNavToggle', onNavToggle);
     <div
         class="default-layout-wrapper min-[600px]:max-w-lg h-full max-[600px]:w-full min-[600px]:max-h-[910px] min-[600px]:aspect-[9/16] overflow-hidden min-[600px]:p-8">
         <div
-            class="default-layout-rounded-wrapper h-full min-[600px]:rounded-[32px] min-[600px]:p-[2px] bg-gradient-to-br from-[#6D3474] via-30% via-[#1B1F28] to-[#2C323A] shadow-2xl shadow-black">
+            class="default-layout-rounded-wrapper h-full min-[600px]:rounded-[32px] min-[600px]:p-[2px] bg-gradient-to-br from-[#6D3474] via-30% via-[#1B1F28] to-[#2C323A] -shadow-2xl -shadow-black">
             <div
                 class="default-layout-rounded-child-wrapper flex flex-col relative show-cursor hover:cursor-auto bg-gradient-to-br from-[#1B1F2899] to-40% to-[#1B1F28dd] h-full w-full overflow-hidden backdrop-blur min-[600px]:rounded-[30px]">
                 <LayoutNavViewWrapper>
